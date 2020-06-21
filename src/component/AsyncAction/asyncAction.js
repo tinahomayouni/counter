6e57581f5ea70403c377940b3f31d85e6d415d77
Tina <tinahomayouni@gmail.com>
2020-06-21 17:11:47 +0430

    add menu to asyncAction component

--- src/component/AsyncAction/asyncAction.js
+++ src/component/AsyncAction/asyncAction.js
@@ -1,10 +1,18 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom';
+import Header from "../../container/header";
 function AsyncAction() {
     return (
+        <>
+        <Header>
+            <nav className="header-about">
+            <Link to="/">Home</Link>
+            </nav>
+        </Header>
         <div>
             AsyncAction
         </div>
+        </>
     )
 }
 
